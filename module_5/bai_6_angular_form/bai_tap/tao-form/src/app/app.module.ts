775518d5a421d50e127821ee1f3fbcd5ebdcc393
A0321I1 - Hồ Quốc Kiên <hquockien@gmail.com>
2022-02-20 23:57:39 +0700

    nop bai

--- module_5/bai_6_angular_form/bai_tap/tao-form/src/app/app.module.ts
+++ module_5/bai_6_angular_form/bai_tap/tao-form/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
