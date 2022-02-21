125bbb63d413d4d620b7a4484e67eb3cd01afdd1
A0321I1 - Hồ Quốc Kiên <hquockien@gmail.com>
2022-02-21 09:14:33 +0700

    nop bai

--- module_5/bai_6_angular_form/bai_tap/tao-form/src/app/app.module.ts
+++ module_5/bai_6_angular_form/bai_tap/tao-form/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { RegisterFinalComponent } from './register-final/register-final.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegisterFinalComponent,
+    LoginComponent
   ],
     imports: [
         BrowserModule,
